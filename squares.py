6dd8f52c6196e2d035e538e469e43fe4842c8081
SANTEOWEN <101460774+SANTEOWEN@users.noreply.github.com>
2023-03-08 22:48:31 +0800

    I learned how to import other functions into other system by literally importing them on a certain part of the system

---
+++ squares.py
@@ -0,0 +1,14 @@
+
+#from functions.py i would like to import a especific function named square to use on my other program
+from functions import square
+
+# u can also especificy a certain module by calling it out especificaly like functions.square inside the value
+for i in range(10):
+    print(f"lamaw{i} is {square(i)}")
+
+
+
+
+
+
+
