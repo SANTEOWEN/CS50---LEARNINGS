a8e37a39f924228f29f7399a9fb08d0cf84140d2
SANTEOWEN <101460774+SANTEOWEN@users.noreply.github.com>
2023-03-08 22:32:09 +0800

    I learned about the conditions statements on python

---
+++ conditions.py
@@ -0,0 +1,15 @@
+#Greater than >
+#Less than <
+
+#we need to especify what data type we use on taking some inputs
+n = int(input("Number: "))
+
+if n > 0:
+    print("N is positive")
+elif n < 0:
+    print("N is negative")
+else:
+    print("N is Zero")
+
+#TypError means theres a mismatch of data type(type)
+
