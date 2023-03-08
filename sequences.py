657d95dbb57acd76a4f622c0d34e5113cda78ac2
SANTEOWEN <101460774+SANTEOWEN@users.noreply.github.com>
2023-03-08 22:33:22 +0800

    I learned about the data structures like the list, tuple, sets, and dictionaries

---
+++ sequences.py
@@ -0,0 +1,35 @@
+#------- DATA STRUCTURES -------#
+
+#List - sequence of mutable values
+#Mutable values - means you change any values in the list
+
+#Tuple - sequence of immutable values
+#Immutable values - this values cannot change u need to create another tuple to change something
+
+#Set - collection of unique values
+#Set - it does not collect in particular order (its just a collection every value needs to be unique)
+
+#Dict - collection of key-value pairs
+#Dict - it was like literally a dictionary(when u look a value u get some other value)
+#Dict - if u find a especific key you will get another value
+
+
+
+
+
+
+
+
+#name = "Harry"
+#we start the every sequence with 0
+#what are we doing here is indexing
+#print(name[1])
+
+#List = this one is called list. we created a variable name (name) with the list of strings inside of it 
+#name = ["Harry", "Ron",  "Lamaw"]
+#print(name[0])
+
+#Tuples 
+#coordinate = (10.0 , 20.0)
+#print(coordinate[0])
+
