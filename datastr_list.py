b8c93d678cd5cc02cd1a42a893b18211b85c6f07
SANTEOWEN <101460774+SANTEOWEN@users.noreply.github.com>
2023-03-08 22:34:14 +0800

    I learned about the data structure list and its basic methods

---
+++ datastr_list.py
@@ -0,0 +1,12 @@
+# Define a list of names
+
+names = ["Harry", "Owen", "Lamaw", "Yawa" ]
+
+
+#append() use to add a a value to end of the existing list
+names.append("Meow")
+
+#sort() use to sort the values on the list
+names.sort()
+
+print(names)
