580c0904318ced7695f8fe6794ddd5cf0e70ff7b
SANTEOWEN <101460774+SANTEOWEN@users.noreply.github.com>
2023-03-08 22:36:02 +0800

    This data structure is kinda hard to understand but i learned about it you input the key then you will get a value

---
+++ dictionaries.py
@@ -0,0 +1,19 @@
+#heres a example of a dictionary 
+
+#you created a first value to especify things then inside it u put another value to input something
+# if u look at "harry" u got a the value called "kamaw"
+houses = {"harry": "kamaw", "draco": "yawa" }
+
+houses["yawa"] = "kamaw"
+
+#in this print statement we look up for a value named harry and inside the value harry it contains a value name "kamaw"
+print(houses["harry"])
+
+
+
+
+
+
+
+
+
