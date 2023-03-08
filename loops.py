0a8af49cb3cb028b77333394a9949ccd6f0d23a4
SANTEOWEN <101460774+SANTEOWEN@users.noreply.github.com>
2023-03-08 22:37:56 +0800

    I learned about loops like the the basic one which is the for loop

---
+++ loops.py
@@ -0,0 +1,14 @@
+
+#range() = is used to give a especific range inside of the sequence
+for i in range(6):
+    print(i)
+
+lamaw = ["lamaw", "yawa", "Fatherless"]
+for name in lamaw:
+    print(name)
+
+named = "lamaw"
+
+for i in name:
+    print(i)
+
